05cec40649d9189e8f36f8204159b3663b110120
duxiaodong <duxiaodong@darlin.me>
2018-06-29 10:35:27 +0800

    test1

--- git.sh
+++ git.sh
@@ -1,7 +1,13 @@
+set -e
+
 git config --global user.name "circle-bot"
 git config --global user.email "bot@darlin.me"
 
 git status
 git commit -am"bump version"
-git push -q "https://$GITHUB_TOKEN@github.com/doxiaodong/mobx-di.git" test-1
+{
+  git push -q "https://$GITHUB_TOKEN@github.com/doxiaodong/mobx-di.git" test-1 &&
+} || {
+  echo "Push error"
+}
 echo "Push success!!"
