3dc2591180e7a004e96c2705ae34cdd32a2cc108
duxiaodong <duxiaodong@darlin.me>
2018-07-04 16:20:51 +0800

    fix: CHANGELOG lose after generate

--- git.sh
+++ git.sh
@@ -6,5 +6,6 @@ git config --global user.name "circle-bot"
 git config --global user.email "duxiaodong@darlin.me"
 
 git status
-git commit -am"bump version"
+git add -A
+git commit -am"bump version $CIRCLE_TAG [skip ci]"
 git status
